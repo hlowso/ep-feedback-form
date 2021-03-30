5b342eb7e608557f422ad92c167426f45c556680
hlowso <h.canderleigh@gmail.com>
2021-03-30 10:54:49 -0400

    preparing parsed csv data via clean(); added S function for determining result of pairing;

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import {parse} from './parse-csv'
+import {parse, clean} from './parse-csv'
+import {E, R} from './rating'
+
+const S = (songs: [number, number], preferred: number, idx: number) => {
+    if (preferred === -1)
+        return 0.5
+    return preferred === songs[idx]
+        ? 1 : 0 
+}
 
 (async () => {
-    const data = await parse('./feedback.csv')
-    for (const row of data) {
-
-    }
+    const raw = await parse('./feedback.csv')
+    const data = clean(raw)
+    for (const result of data)
+        console.log(result)
 })()
--- src/parse-csv.ts
+++ src/parse-csv.ts
@@ -12,3 +12,25 @@ export const parse = async (path: string) => {
     })
     return array
 }
+
+const trim = (str?:string) => str ? str.substr(1, str.length - 2) : undefined
+
+const value = (str: string, i: number) => {
+    switch (i) {
+        case 0: return trim(str)!.split('-').map(Number)
+        case 1: return parseInt(trim(str)!)
+        case 2: return trim(str)!.split('-')
+        default: return trim(str)
+    }
+}
+
+export const clean = (data: Array<string[]>) => {
+    const columns = data.shift()!
+    const cleanRow = (row: string[]) =>
+        row.reduce((obj, curr, i) => {
+            if (i < 6)
+                obj[trim(columns[i])!] = value(curr, i)
+            return obj
+        }, {})
+    return data.map(cleanRow)
+}
